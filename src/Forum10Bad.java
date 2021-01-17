b3fdb92e02c353db030fe8a3761b3cd050251cb3
Iqbal Fauzan <v_iqbal.fauzan@anteraja.id>
2021-01-17 11:25:54 +0700

    Forum 10 BAD - Thread 1

---
+++ src/Forum10Bad.java
@@ -0,0 +1,48 @@
+public class Forum10Bad {
+    public static void main(String[] args) {
+        try {
+            melemparkanException();
+        } catch (Exception ex) {
+            System.err.println("Exception yang terlempar dari blok catch pada "
+                    + "line 7 ditangani di main method");
+        }
+
+        tidakMelemparkanException();
+    }
+
+    public static void tidakMelemparkanException() {
+        try {
+            System.out.println("Method tidakMelemparkanException dijalankan");
+        }
+        //blok catch ini tidak akan dieksekusi karena method tidak melemparkan exception
+        catch (Exception ex) {
+            System.err.println(ex);
+        } finally {
+            System.err.println("Pernyataan dalam blok finally pada line 22 dieksekusi");
+        }
+
+        System.out.println("Akhir dari method tidakMelemparkanException");
+
+    }
+
+    public static void melemparkanException() throws Exception {
+
+        try {
+            System.out.println("Pernyataan pada line 31 dieksekusi");
+            throw new Exception();
+        } catch (Exception ex) {
+            System.out.println("Exception yang terlempar dari "
+                    + "method melemparkanException telah ditangkap oleh blok catch pada line 36");
+            System.err.println("Caught Exception");
+            System.err.println("getMessage():" + ex.getMessage());
+            System.err.println("getLocalizedMessage():"
+                    + ex.getLocalizedMessage());
+            System.err.println("toString():" + ex);
+            System.err.println("printStackTrace():");
+            ex.printStackTrace();
+            throw ex;//exception ini akan ditangani oleh blok catch di main method
+        } finally {
+            System.err.println("Pernyataan dalam blok finally pada line 41 dieksekusi");
+        }
+    }
+}
