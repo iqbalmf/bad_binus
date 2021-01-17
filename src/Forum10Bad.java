75fa8ac9aeef06ce6aedc84469e07dac06a9ad84
Iqbal Fauzan <v_iqbal.fauzan@anteraja.id>
2021-01-17 11:39:11 +0700

    Forum 10 BAD - Thread 2

--- src/Forum10Bad.java
+++ src/Forum10Bad.java
@@ -1,13 +1,21 @@
 public class Forum10Bad {
     public static void main(String[] args) {
         try {
-            melemparkanException();
+//            melemparkanException();
         } catch (Exception ex) {
             System.err.println("Exception yang terlempar dari blok catch pada "
                     + "line 7 ditangani di main method");
         }
+//        tidakMelemparkanException();
 
-        tidakMelemparkanException();
+        try {
+            System.out.println(OverloadingException.method(10, 20.0));
+            System.out.println(OverloadingException.method(10.0, 20));
+            System.out.println(OverloadingException.method(10.0, 20.0));
+            System.out.println(OverloadingException.method(10));
+        }catch (Exception exception){
+            System.out.println("exception : "+exception);
+        }
     }
 
     public static void tidakMelemparkanException() {
@@ -20,13 +28,11 @@ public class Forum10Bad {
         } finally {
             System.err.println("Pernyataan dalam blok finally pada line 22 dieksekusi");
         }
-
         System.out.println("Akhir dari method tidakMelemparkanException");
 
     }
 
     public static void melemparkanException() throws Exception {
-
         try {
             System.out.println("Pernyataan pada line 31 dieksekusi");
             throw new Exception();
@@ -43,6 +49,24 @@ public class Forum10Bad {
             throw ex;//exception ini akan ditangani oleh blok catch di main method
         } finally {
             System.err.println("Pernyataan dalam blok finally pada line 41 dieksekusi");
+        }
+    }
+
+    public static class OverloadingException {
+        static double method(int i) throws Exception {
+            return i / 0;
+        }
+
+        static boolean method(boolean b) {
+            return !b;
+        }
+
+        static double method(int x, double y) throws Exception {
+            return x + y;
+        }
+
+        static double method(double x, double y) {
+            return x + y - 3;
         }
     }
 }
